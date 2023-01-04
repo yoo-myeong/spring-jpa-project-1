711ba97539a9a3ef4e5b1a7e811096ac1a7ebce8
sceonn <yoou201@naver.com>
2023-01-05 08:10:25 +0900

    fix : 엔티티 설계 최적화

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -31,10 +31,16 @@ public class Category {
     )
     private List<Item> items;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+
+    }
 }
